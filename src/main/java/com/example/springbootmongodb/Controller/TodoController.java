7e87514135a483681d867affc17871c53756e337
angkhanalil <34016307+angkhanalil@users.noreply.github.com>
2023-04-14 17:38:36 +0700

    feat: update & delete by id

--- src/main/java/com/example/springbootmongodb/Controller/TodoController.java
+++ src/main/java/com/example/springbootmongodb/Controller/TodoController.java
@@ -2,12 +2,16 @@ package com.example.springbootmongodb.Controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +43,44 @@ public class TodoController {
       return new ResponseEntity<TodoDTO>(todo, HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GetMapping("/todos/{id}")
+  public ResponseEntity<?> getSingleTodo(@PathVariable("id") String id) {
+    Optional<TodoDTO> todoOptional = todoRepository.findById(id);
+    if (todoOptional.isPresent()) {
+      return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>("Todo not found with id:" + id, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @PutMapping("/todos/{id}")
+  public ResponseEntity<?> updateById(@PathVariable("id") String id, @RequestBody TodoDTO todo) {
+    Optional<TodoDTO> todoOptional = todoRepository.findById(id);
+    if (todoOptional.isPresent()) {
+      TodoDTO todoToSave = todoOptional.get();
+      todoToSave.setCompleted(todo.getCompleted() != null ? todo.getCompleted() : todoToSave.getCompleted());
+      todoToSave.setTodo(todo.getTodo() != null ? todo.getTodo() : todoToSave.getTodo());
+      todoToSave.setDescription(todo.getDescription() != null ? todo.getDescription() : todoToSave.getDescription());
+      todoToSave.setUpdatedAt(new Date(System.currentTimeMillis()));
+      todoRepository.save(todoToSave);
+      return new ResponseEntity<>(todoToSave, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>("Todo not found with id:" + id, HttpStatus.NOT_FOUND);
+    }
+
+  }
+
+  @DeleteMapping("/todos/{id}")
+  public ResponseEntity<?> deleteById(@PathVariable("id") String id) {
+    try {
+      todoRepository.deleteById(id);
+      return new ResponseEntity<>("Success deleted with id:" + id, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+
     }
   }
 
